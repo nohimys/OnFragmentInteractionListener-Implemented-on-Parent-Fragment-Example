284cd6ce702636b82cacd7d3c5d8097f4a7ad1c1
Nohim Sandeepa <itsnohim@gmail.com>
2017-10-08 01:20:01 +0530

    Implemented two callback interfaces in ChildFragment class. One for ParentFragment to implement & one for MainActivity to implement.

--- app/src/main/java/com/nohimys/onfragmentinteractionlistenerimplementedonparentfragmentexample/ChildFragment.java
+++ app/src/main/java/com/nohimys/onfragmentinteractionlistenerimplementedonparentfragmentexample/ChildFragment.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ChildFragment.OnFragmentInteractionListener} interface
+ * {@link OnChildFragmentInteractionByParentFragmentListener} interface
  * to handle interaction events.
  * Use the {@link ChildFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -29,7 +29,8 @@ public class ChildFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private OnChildFragmentInteractionByParentFragmentListener onChildFragmentInteractionByParentFragmentListener;
+    private OnChildFragmentInteractionByActivityListener onChildFragmentInteractionByActivityListener;
 
     private Button buttonMessageToParentFragment;
     private Button buttonMessageToActivity;
@@ -96,40 +97,34 @@ public class ChildFragment extends Fragment {
 
     // TODO: Rename method, update argument and hook method into UI event
 //    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
+//        if (onChildFragmentInteractionByParentFragmentListener != null) {
+//            onChildFragmentInteractionByParentFragmentListener.onChildFragmentInteraction(uri);
 //        }
 //    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
+//        if (context instanceof OnChildFragmentInteractionByParentFragmentListener) {
+//            onChildFragmentInteractionByParentFragmentListener = (OnChildFragmentInteractionByParentFragmentListener) context;
 //        } else {
 //            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
+//                    + " must implement OnChildFragmentInteractionByParentFragmentListener");
 //        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        onChildFragmentInteractionByParentFragmentListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+
+    public interface OnChildFragmentInteractionByParentFragmentListener {
+        void onChildFragmentInteraction(Uri uri);
+    }
+
+    public interface OnChildFragmentInteractionByActivityListener {
+        void onChildFragmentInteraction(Uri uri);
     }
 }
